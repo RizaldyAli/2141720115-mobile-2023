6ea127ac7d9292b15c75e351bde7b516da81ea80
Rizaldy Ali Machfuddin <alirizaldy357@gmail.com>
2023-11-27 22:30:22 +0700

    W13: Jawaban Soal 8

--- week13/src/stream_rizaldy/lib/main.dart
+++ week13/src/stream_rizaldy/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'stream.dart';
 import 'dart:async';
@@ -36,24 +38,34 @@ class _StreamHomePageState extends State<StreamHomePage> {
   int lastNumber = 0;
   late StreamController numberStreamController;
   late NumberStream numberStream;
+  late StreamTransformer transformer;
 
   @override
   void initState() {
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
-      setState(() {
-        lastNumber = event;
-      });
-    }).onError((error) {
-      setState(() {
-        lastNumber = -1;
-      });
-    });
+    // stream.transform(transformer).listen((event) {
+    //   setState(() {
+    //     lastNumber = event;
+    //   });
+    // }).onError((error) {
+    //   setState(() {
+    //     lastNumber = -1;
+    //   });
+    // });
     // colorStream = ColorStream();
     // changeColor();
     super.initState();
+
+    transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
   }
 
   @override
@@ -76,7 +88,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
   }
 
   void addRandomNumber() {
-    Random random = Random();
+    // Random random = Random();
     // int myNum = random.nextInt(10);
     // numberStream.addNumberToSink(myNum);
     numberStream.addError();
